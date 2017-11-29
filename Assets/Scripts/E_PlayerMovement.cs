75fd7235dfe71b7973a40052f132eadd04a9fb1a
Albert Dols Ferrandis <al315151@uji.es>
2017-11-29 15:52:54 +0100

    Copia Scripts pa entrenar a lo rocky
    chan chan chaaaaaaan chan chan chaaaaaan chan chan chaaaaan chan chan
    chaaaaaaaaaaaaaaan...

---
+++ Assets/Scripts/E_PlayerMovement.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class E_PlayerMovement : MonoBehaviour {
+
+    public string horizontal;
+    public string vertical;
+    public GameObject rival;
+    public Slider vida;
+    public Transform[] fila1 = new Transform[3];
+    public Transform[] fila2 = new Transform[3];
+    public Transform[] fila3 = new Transform[3];
+    private Transform[,] positions = new Transform[3,3];
+    private int posX = 1;
+    private int posY = 1;
+    private float timer = 0.1f;
+    private int life;
+    private int shield;
+    private int chargues;
+    private bool defense = false;
+
+	public bool myTurn = true;
+	public bool legalMove; // SE ACTUALIZA EN MOVE, AUNQUE ESTA DEUELVA VOID
+
+	public enum playerActions
+	{
+		Charge, 
+		Shoot,
+		Move,
+		Guard
+	};
+	public enum playerMovements
+	{
+		MoveUp,
+		MoveRight,
+		MoveLeft,
+		MoveDown
+	};
+
+    // Use this for initialization
+    void Start () {
+        life = 3;
+        shield = 3;
+        chargues = 1;
+        for (int i = 0; i < fila1.Length; i++) {
+
+            positions[0, i] = fila1[i];
+            positions[1, i] = fila2[i];
+            positions[2, i] = fila3[i];
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myTurn) {
+			if (timer >= 3.0f) 
+			{
+				float random = Random.value;
+
+				if( random < 0.4f)
+					{
+						if (random < 0.1f) 
+						{
+							 Move (playerMovements.MoveUp);
+						} 
+						else if (random < 0.2f) 
+						{
+							Move (playerMovements.MoveLeft);
+						}
+						else if (random < 0.3f) 
+						{
+							Move (playerMovements.MoveRight);
+						} 
+						else
+						{
+							Move (playerMovements.MoveDown);
+						}
+						
+					}
+				else if( random < 0.6f)
+					{
+						defense = false;
+						Attack ();
+					}
+				else if( random < 0.8f)
+					{
+						defense = false;
+						Rechargue ();
+						
+					}
+				else
+					{
+						if (shield > 0) {	
+							defense = true;
+							shield = shield - 1;
+						}
+						print ("Escudos: " + shield);
+						
+					}
+
+				timer = 0.0f;
+			}
+				timer = timer + Time.deltaTime;			
+		}
+
+    }
+
+	void Move(playerMovements movement) {
+
+		switch (movement) 
+		{
+		case playerMovements.MoveDown:
+			{
+				posY = posY - 1;	
+				break;
+			}
+		case playerMovements.MoveLeft:
+			{
+				posX = posX - 1;
+				break;
+			}
+		case playerMovements.MoveRight:
+			{
+				posX = posX + 1;
+				break;
+			}
+		case playerMovements.MoveUp:
+			{
+				posY = posY + 1;
+				break;
+			}
+		}
+       // posX = posX + (int)(Input.GetAxisRaw(horizontal));
+       // posY = posY + (int)-(Input.GetAxisRaw(vertical));
+		if (posX < 0) { posX = 0; legalMove = false; }
+		else if (posX > 2) { posX = 2; legalMove = false; }
+		if (posY < 0) { posY = 0; legalMove = false;}
+		else if (posY > 2) { posY = 2; legalMove = false; }
+        transform.position = positions[posX, posY].position;
+		if (legalMove) 
+		{
+			legalMove = true;
+		}
+    }
+    void Attack() {
+
+        float distanceToEnemy = Vector3.Distance(rival.transform.position, transform.position);
+        print("Distancia: " + distanceToEnemy); 
+        if(chargues > 0 && distanceToEnemy <= 7.5f )
+        {
+            rival.GetComponent<PlayerMovement>().Damage(1);
+            chargues = chargues - 1;
+        }
+
+
+    }
+
+    void Rechargue() {
+
+
+        if (chargues < 5) {
+
+            chargues = chargues + 1;
+            print("Cargas "+chargues);
+        }
+
+    }
+    public void Damage(int damage) {
+
+        if (!defense)
+        {
+            life = life - damage;
+            print("Vida " + life);
+        }
+       
+    }
+
+}
---
+++ Assets/Scripts/E_TurnBasedBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_TurnBasedBehaviour : MonoBehaviour {
+
+	private int currentTurn;
+
+	//Gestion de partida: turnos, gameflow...
+
+	public GameObject[] playerReferences;
+
+
+	//Hacer la funcion que hace la foto.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gamePaceManager ();		
+
+		if (Input.GetKeyDown (KeyCode.F1)) 
+		{
+			Debug.Log ("Es turno del jugador 1");
+			enableTurn (0);
+			disableTurn (1);
+		}
+		if (Input.GetKeyDown (KeyCode.F2)) 
+		{
+			Debug.Log ("Es turno del jugador 2");
+			enableTurn (1);
+			disableTurn (0);
+		}
+
+
+	}
+
+	void gamePaceManager()
+	{
+		// Primero, pediremos los inputs de cada jugador.
+		// Después, calcularemos el peso que tiene la acción del enemigo
+		//siguiendo las variables correspondientes.
+		// Por último, actualizaremos estado actual de los jugadores.
+		// Si en esta actualizacion hay Game Over (se acaban los turnos o alguien muere)
+		// Se pasa a pantalla de fin de partida.
+
+
+	}
+
+
+	public void enableTurn(int jugador)
+	{
+		playerReferences [jugador].GetComponent<PlayerMovement> ().myTurn = true;
+	}
+
+	public void disableTurn(int jugador)
+	{
+		playerReferences [jugador].GetComponent<PlayerMovement> ().myTurn = false;
+	}
+
+
+}
