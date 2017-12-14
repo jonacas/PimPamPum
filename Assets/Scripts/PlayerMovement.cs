e494a7a2f23c35232b95f23b0418dc64c20c7823
Jonatan <al315616@uji.es>
2017-12-14 12:55:05 +0100

    Tool
    +Cambios en el script de PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -44,28 +44,12 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		get {
 			return life;
-			}
+		}
 	}
 
 	
     // Use this for initialization
     void Start () {
-
-        matQ = new GestionDeArchivos<MatrizQ>("MatrizQ" + System.Convert.ToString(GlobalData.Dificultad));
-
-        if (GlobalData.Dificultad >= 7)
-        {
-            decisionadorIA = new Decisionador(ModoDecisionador.ProPlayer, matQ.objeto);
-        }
-
-        else if (GlobalData.Dificultad >= 4)
-        {
-            decisionadorIA = new Decisionador(ModoDecisionador.Normal, matQ.objeto);
-        }
-        else
-        {
-            decisionadorIA = new Decisionador(ModoDecisionador.Manco, matQ.objeto);
-        }
 
         life = 3;
         shield = 3;
@@ -77,133 +61,17 @@ public class PlayerMovement : MonoBehaviour {
             positions[2, i] = fila3[i];
 
         }
-
-		IAPhase = false;
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (myTurn) {
-			//RandomCalculation ();
-			}
-				timer = timer + Time.deltaTime;			
-		if (ActionButtons.Length > 0) 
-		{
-			if (timerBetweenTurns > targetTimeBetweenTurns && ActionButtons [0].interactable == false) 
-			{
-				EnableActionButtons ();
-			}
-			else
-			{
-				timerBetweenTurns += Time.deltaTime;
-			}
-		}
-
-		distanceToEnemy = Vector3.Distance(rival.transform.position, transform.position);
-    }
-
-	public void DisableActionButtons()
-	{
-		for (int i = 0; i < ActionButtons.Length; i++) 
-		{
-			ActionButtons [i].interactable = false;
-		}
 	}
 
-	public void EnableActionButtons()
-	{
-		for (int i = 0; i < ActionButtons.Length; i++) 
-		{
-			ActionButtons [i].interactable = true;
-		}
-	}
 
-	public void RandomCalculation()
-	{
-	if (timer >= 1.0f) 
-	{
-		float random = Random.value;
+	void Move(int move) { //Funcion para mover al personaje
 
-		if( random < 0.4f)
-		{
-			if (random < 0.1f) 
-			{
-                Move(1);//playerActions.MoveUp);
-			} 
-			else if (random < 0.2f) 
-			{
-                Move(2); //playerActions.MoveLeft);
-			}
-			else if (random < 0.3f) 
-			{
-                Move(3); //(playerActions.MoveRight);
-			} 
-			else
-			{
-                Move(4); //(playerActions.MoveDown);
-			}
-
-		}
-		else if( random < 0.6f)
-		{
-			defense = false;
-			Attack ();
-		}
-		else if( random < 0.8f)
-		{
-			defense = false;
-			Rechargue ();
-
-		}
-		else
-		{
-            Defense();
-
-		}
-
-		timer = 0.0f;
-	}
-}
-
-	public void UpdateNumberOfTurns()
-	{
-		totalTurns = totalTurns + 1;
-		if (canvasReference != null) 
-		{
-			canvasReference.UpdateTurnNumberCanvas (totalTurns);
-		}
-		if (ActionButtons.Length > 0) 
-		{
-			DisableActionButtons ();
-		}
-		timerBetweenTurns = 0.0f;
-
-		IAPhase = true;
-		IAMovementAllowed = false;
-		//AÑADIR AQUI EL LANZAMIENTO DEL MOVIMIENTO DE LA IA
-
-        /*while (!rival.GetComponent<PlayerMovement>().IAMovmentAllowed) 
-        {
-            accionIA = decisionadorIA.decidirMovimiento(GetEstado());
-        }*/
-
-
-
-        //AQUI FINALIZA EL LANZAMIENTO DEL MOVIMIENTO DE LA IA
-		IAPhase = false;
-
-	}
-
-	public void Move(int move) {
-
-        //ARRIBA Y ABAJO ESTAN AL REVES!!!!
 
 		switch ( move /*movement*/) 
 		{
 		case 1:    //playerActions.MoveDown:
 			{
-				posY = posY - 1;	
+				posY = posY + 1;	
 				break;
 			}
         case 2:      //playerActions.MoveLeft:
@@ -218,7 +86,7 @@ public class PlayerMovement : MonoBehaviour {
 			}
         case 4: //playerActions.MoveUp:
 			{
-				posY = posY + 1;
+				posY = posY - 1; //Cambiado para tenerlo en cuenta, va en base a la matriz
 				break;
 			}
         default:
@@ -227,91 +95,31 @@ public class PlayerMovement : MonoBehaviour {
                 break;
             }
 		}
-       // posX = posX + (int)(Input.GetAxisRaw(horizontal));
-       // posY = posY + (int)-(Input.GetAxisRaw(vertical));
-		legalMove = true;
-		if (posX < 0) 
-		{ posX = 0; legalMove = false;  IAMovementAllowed = false; /*RandomCalculation ()*/ }
-		else if (posX > 2) 
-		{ posX = 2; legalMove = false;  IAMovementAllowed = false; /*RandomCalculation ()*/ }
-		if (posY < 0)
-		{ posY = 0; legalMove = false;  IAMovementAllowed = false;/*RandomCalculation ()*/}
-        else if (posY > 2)
-		{ posY = 2; legalMove = false;  IAMovementAllowed = false; /*RandomCalculation ()*/ }
+    }
+
+    void Bazoonga() {
+
+        chargues = 0;
+
+
+    }
+    void Attack() {
+
+        
+        chargues = chargues - 1;
+
+    }
+
+    void Rechargue() {
+        chargues = chargues + 1;
        
-		transform.position = positions[posX, posY].position;
-		if (legalMove) 
-		{
-			legalMove = true;
-			IAMovementAllowed = true;
-			if (!IAPhase) {
-				UpdateNumberOfTurns ();
-			}
-		}
-
-
-
-    }
-    public void Attack() {
-
-		if (chargues <= 0) {
-			//RandomCalculation ();
-		} 
-		else
-		{
-			 
-			print("Distancia: " + distanceToEnemy); 
-			if(chargues > 0 && distanceToEnemy <= 7.5f )
-			{
-				if (chargues == 5)
-				{
-					rival.GetComponent<PlayerMovement> ().Damage (2);
-					chargues = 0;
-				}
-				else
-				{
-					rival.GetComponent<PlayerMovement>().Damage(1);
-					chargues = chargues - 1;
-				}
-				if (!IAPhase) {
-					UpdateNumberOfTurns ();
-				}
-				IAMovementAllowed = true;
-			}
-		
-		}
-		IAMovementAllowed = false;
-
-    }
-
-    public void Rechargue() {
-
-
-        if (chargues < 5) {
-
-            chargues = chargues + 1;
-            print("Cargas "+chargues);
-        }
-		else if (chargues == 5) 
-		{
-			print ("Carga máxima conseguida");
-		}
-
-		if (!IAPhase) {
-			UpdateNumberOfTurns ();
-		}
-		IAMovementAllowed = true;
-
     }
     public void Damage(int damage) {
-		if (damage == 2) 
-		{
-			life = 0;
-		}
-        else if (!defense)
+		
+        if (!defense)
         {
             life = life - damage;
-            print("Vida " + life);
+            
         }
 		if (canvasReference != null && lifeSlider != null) 
 		{
@@ -321,67 +129,41 @@ public class PlayerMovement : MonoBehaviour {
 
     public void Defense() {
 
-		if (shield > 0) {
-			defense = true;
-			shield = shield - 1;
-			if (!IAPhase) 
-			{
-				UpdateNumberOfTurns ();
-				IAMovementAllowed = true;
-			}
-		}
-		else
-		{
-			IAMovementAllowed = false;
-		}
+        shield = shield - 1;
     }
 
-	public void PlayerActionToMovement(playerActions action)
+	public void EjecutarAccion(playerActions action)
 	{
 		switch (action) 
 		{
 		case playerActions.Charge:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Rechargue ();
-				break;
+                    Rechargue();break;
 			}
 		case playerActions.Guard:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Defense ();
-				break;
+                    Defense();break;
 			}
 		case playerActions.Shoot:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Attack ();
-				break;
+                    Attack();break;
 			}
 		case playerActions.MoveDown:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Move (1);
-				break;
+                    Move(1);break;
 			}
 		case playerActions.MoveLeft:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Move (2);
-				break;
-			}
+                    Move(2); break;
+                }
 		case playerActions.MoveUp:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Move (4);
-				break;
-			}
+                    Move(4); break;
+                }
 		case playerActions.MoveRight:
 			{
-				rival.GetComponent<PlayerMovement> ().IAPhase = true;
-				rival.GetComponent<PlayerMovement>().Move (3);
-				break;
-			}
+                    Move(3); break;
+                }
 		}
 	}
 
